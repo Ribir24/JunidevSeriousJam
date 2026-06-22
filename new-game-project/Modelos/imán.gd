fb452e8004501c51858cd889b2099831461e1e51
Víctor Martínez Moreno <vicmar20@ucm.es>
2026-06-22 14:21:47 +0200

    Movimiento de imán

--- new-game-project/Modelos/imán.gd
+++ new-game-project/Modelos/imán.gd
@@ -4,6 +4,10 @@ extends Node3D
 @export var min_x: float = -3.0
 @export var max_x: float = 3.0
 
-func _input(event):
+func _process(delta: float):
 	if Input.is_action_pressed("LeftInput"):
-		print("left")
+		var direccion = -global_transform.basis.z
+		global_position += direccion * speed * delta
+	if Input.is_action_pressed("RightInput"):
+		var direccion = global_transform.basis.z
+		global_position += direccion * speed * delta

198971c7396e6ae322607f48579b51995510be93
Víctor Martínez Moreno <vicmar20@ucm.es>
2026-06-27 00:45:18 +0200

    Añadido aceite a minjuego de escaner

--- new-game-project/Scenes/Escaner/minijuego_escaner.gd
+++ new-game-project/Scenes/Escaner/minijuego_escaner.gd
@@ -3,7 +3,7 @@ extends Node3D
 const DIST:float=1000.0
 var mouse:Vector2
 var grabbed_object:Node3D=null
-
+var fin:bool = false
 
 @export var min_x:float
 @export var max_x:float
@@ -12,10 +12,13 @@ var grabbed_object:Node3D=null
 
 var scan_amount:int=0
 
+func _ready() -> void:
+	scanner.position.x = min_x;
+
 func _process(_delta: float) -> void:
 	if grabbed_object==scanner:
-		scanner.global_position.x=InteractManager.get_grab_position(mouse,scanner.global_position.z)-20
-		print(scanner.global_position)
+		scanner.global_position.x=InteractManager.get_grab_position(mouse,scanner.global_position.z)-20.5
+		#print(scanner.global_position)
 		if scanner.global_position.x<min_x:
 			scanner.global_position.x=min_x
 		elif scanner.global_position.x>max_x:
@@ -25,7 +28,7 @@ func _process(_delta: float) -> void:
 func _input(event):
 	if event is InputEventMouseMotion:
 		mouse=event.position
-	if event is InputEventMouseButton and event.pressed and event.button_index == 1:
+	if event is InputEventMouseButton and event.pressed and event.button_index == 1 and !fin:
 		grabbed_object=InteractManager.get_mouse_world_pos(mouse,DIST)
 
 func _check_bar():
@@ -37,5 +40,7 @@ func _check_bar():
 		scan_amount+=1
 	else:
 		scanner.play_anim_string("FINAL")
+		grabbed_object = null;
+		fin = true;
 		await get_tree().create_timer(1.5).timeout
 		print("FINAL")

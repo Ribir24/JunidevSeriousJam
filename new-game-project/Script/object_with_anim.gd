b7a4980149dcc6d7fe3ddeea4213f1ae74fe4cc5
Adrián de Miguel Cerezo <adriadem@ucm.es>
2026-06-26 12:16:56 +0200

    inicio 2 con puerta

--- new-game-project/Script/object_with_anim.gd
+++ new-game-project/Script/object_with_anim.gd
@@ -5,12 +5,21 @@ extends Node
 @export var anim_name:String
 var anim_finished:bool=false
 
+func _ready() -> void:
+	anim_player.animation_finished.connect(_on_anim_finish)
+
 func appear():
 	anim_player.play(anim_name)
-	anim_player.animation_finished.connect(_on_anim_finish)
+	
+
+func play_anim_string(anim:String):
+	anim_player.play(anim)
 
 func is_anim_finished()->bool:
 	return anim_finished
 
 func _on_anim_finish(_anim_name: StringName) -> void:
 	anim_finished=true
+
+func speed_up():
+	anim_player.speed_scale=1000
